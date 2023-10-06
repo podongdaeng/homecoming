e12dcfabe30819ae65f9f16dc7def029601b71c4
SF93 <kdman98@naver.com>
2023-10-06 11:33:24 +0900

    git init

---
+++ src/main/kotlin/podongdaeng/homecoming/HomecomingApplication.kt
@@ -0,0 +1,11 @@
+package podongdaeng.homecoming
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomecomingApplication
+
+fun main(args: Array<String>) {
+	runApplication<HomecomingApplication>(*args)
+}
---
+++ src/test/kotlin/podongdaeng/homecoming/HomecomingApplicationTests.kt
@@ -0,0 +1,13 @@
+package podongdaeng.homecoming
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HomecomingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

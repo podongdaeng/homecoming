01ac93eda245a85d73878a901ad7f0ad4366e16e
SF93 <kdman98@naver.com>
2023-10-31 20:01:06 +0900

    Update BasicController.kt

--- src/main/kotlin/podongdaeng/homecoming/controller/BasicController.kt
+++ src/main/kotlin/podongdaeng/homecoming/controller/BasicController.kt
@@ -7,5 +7,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class MessageController {
     @GetMapping("/hello")
-    fun hello(@RequestParam("name", required = false, defaultValue = "Student") name: String?) = "Hello, $name!"
+    fun hello(@RequestParam("name", required = false, defaultValue = "Student") name: String?): String {
+        
+        return "Hello, $name!"
+    }
 }

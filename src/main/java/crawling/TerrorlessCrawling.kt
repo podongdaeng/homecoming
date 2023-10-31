6635ae489a15894888f76ccaab3e3e52a329b616
SF93 <kdman98@naver.com>
2023-10-31 23:16:22 +0900

    TO HTML request

--- src/main/java/crawling/TerrorlessCrawling.kt
+++ src/main/java/crawling/TerrorlessCrawling.kt
@@ -1,57 +1,49 @@
-import org.openqa.selenium.By
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.WebElement
-import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.chrome.ChromeOptions
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import org.springframework.stereotype.Component
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+@Serializable // TODO: MOVE DATA CLASS TO NEW FILE!
+data class TerrorlessData(
+    val result: ResultData
+) {
+    @Serializable
+    data class ResultData(
+        val data: Data1
+    ) {
+        @Serializable
+        data class Data1(
+            val json: JsonData
+        ) {
+            @Serializable
+            data class JsonData(
+                val threats: List<ThreatData>
+            ) {
+                @Serializable
+                data class ThreatData(
+                    val threat: String? // TODO: 얘 확장해서 HTML 응답에 맞게 쭉~
+                )
+            }
+        }
+    }
+}
 
 @Component
 class TerrorlessCrawling {
-    private val driver: WebDriver
-    private var element: WebElement? = null
-    private val url: String
-    // 웹드라이버 설치경로 설정
-    private val WEB_DRIVER_ID = "webdriver.chrome.driver"
-    private val WEB_DRIVER_PATH = "chromedriver"
-
-    init {
-        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH)
-        val options = ChromeOptions()
-        options.setBinary("")
-        options.addArguments("--start-maximized")
-        options.addArguments("--disable-popup-blocking")
-        options.addArguments("--disable-default-apps")
-        driver = ChromeDriver(options)
-        url = "https://terrorless.01ab.net/"
-    }
-
     fun activateBot() {
-        try {
-            driver.get(url)
-            Thread.sleep(2000)
-            val element = driver.findElement(By.className("css-1qd5env"))
-             element.click()
-            Thread.sleep(2000)
-                driver.findElement(By.xpath("/html/body/div[1]/main/div[2]/div/div/div/div[2]/div[1]/div[1]/div[2]/span"))
-            val count: Int = element.getText().substring(6, element.getText().length).toInt()
-            for (i in 0 until count) {
-                val data =
-                    driver.findElement(By.xpath("/html/body/div[1]/main/div[2]/div/div/div/div[2]/div[" + (i + 2) + "]"))
-                System.out.println(data.getText()+"\n")
-            }
-            /*for(int i=0;i<count;i++)
-            {
-                element=driver.findElement
-                        (By.cssSelector("#react-kakao-maps-sdk-map-container > div:nth-child(1) > div > div:nth-child(6) > div:nth-child("+(i+1)+")"));
-                System.out.println("left:" + element.getCssValue("left")
-                        + " top:" + element.getCssValue("top") + " display:"
-                        + element.getCssValue("display") + "\n");
-            }*/
-            //좌표관련 내용
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            driver.close()
-        }
+        val client = HttpClient.newBuilder().build();
+        val request = HttpRequest.newBuilder()
+            .uri(URI.create("https://api.terrorless.01ab.net/trpc/threat.list"))
+            .build();
+
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        val print = response.body()
+        println(print)
+//        val a = Json.decodeFromString<TerrorlessData>(print)) // TODO: println(a) 가 잘 될지는 모르겠는데 대충 이렇게 객체로 마는데 성공하면 OK
+//        println(a)
     }
 }

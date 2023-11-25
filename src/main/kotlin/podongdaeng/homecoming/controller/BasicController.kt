c28f146333a9841f0e4c474730a1684f6b37c429
maengjae <112834736+maengjae@users.noreply.github.com>
2023-11-25 20:54:10 +0900

    Update BasicController.kt

--- src/main/kotlin/podongdaeng/homecoming/controller/BasicController.kt
+++ src/main/kotlin/podongdaeng/homecoming/controller/BasicController.kt
@@ -1,6 +1,5 @@
 package podongdaeng.homecoming.controller
 
-<<<<<<< Updated upstream
 import org.springframework.beans.factory.annotation.Value
 import podongdaeng.homecoming.model.TerrorlessData
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,7 +13,7 @@ import podongdaeng.homecoming.service.AddressService
 @RestController
 class BasicController(
     private val addressService : AddressService
-){
+) {
     private val terrorlessCrawlingService = BasicService.TerrorlessCrawlingService()
 
     @GetMapping("/near-station")
@@ -51,6 +50,7 @@ class BasicController(
                 }
                 fixedGpsList
             }
+
             2 -> {
                 val randomGpsList = (1..4).map {
                     TestGpsResponse(
@@ -60,6 +60,7 @@ class BasicController(
                 }
                 randomGpsList
             }
+
             3 -> {
                 val singleGps = TestGpsResponse(
                     latitude = gpsLatiLowerLeft + (gpsLatiUpperRight - gpsLatiLowerLeft) / 2,
@@ -67,55 +68,9 @@ class BasicController(
                 )
                 listOf(singleGps)
             }
+
             else -> emptyList()
         }
         return listOfGps
-=======
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.RequestParam
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
-import podongdaeng.homecoming.client.AddressFeignClient
-
-
-@Service
-class AddressService(
-    @Value("\${api.key}") private val apiKey: String,
-) {
-    fun searchNearStationByGps(
-        gpsLati: Double,
-        gpsLong: Double,
-    ): String {
-        val apiUrl =
-            "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCrdntPrxmtSttnList?serviceKey=${apiKey}&pageNo=1&numOfRows=30&_type=json&gpsLati=${gpsLati}&gpsLong=${gpsLong}"
-        val client = HttpClient.newBuilder().build();
-        val request = HttpRequest.newBuilder()
-            .uri(URI.create(apiUrl))
-            .GET()
-            .build();
-
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-        return response.body()
-    }
-}
-
-@RestController
-class AddressController @Autowired constructor(private val addressFeignClient: AddressFeignClient) {
-    @GetMapping("/near-station")
-    fun searchAddress(@RequestParam("gps_lati") gpsLati: String, @RequestParam("gps_long") gpsLong: String): List<GpsCoordinates> {
-        val jsonString = addressService.searchNearStationByGps(gpsLati.toDouble(), gpsLong.toDouble())
-
-        val response = parseJsonResponse(jsonString)
-        val jsonResult=response.response.body.items.item
-
-        return jsonResult.map{busStation -> GpsCoordinates(busStation.nodenm,busStation.gpslati,busStation.gpslong)}
-
->>>>>>> Stashed changes
     }
 }

3893ccc131f0704ee8aa06dc4759aa0edca86321
maengjae <112834736+maengjae@users.noreply.github.com>
2023-11-25 23:08:34 +0900

    db read

--- src/main/kotlin/podongdaeng/homecoming/BasicService.kt
+++ src/main/kotlin/podongdaeng/homecoming/BasicService.kt
@@ -9,32 +9,32 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 class BasicService {
-//    class AddressService(
-//        @Value("\${api.key}") private val apiKey: String,
-//    ) {
-//        fun searchNearStationByGps(
-//            gpsLati: Double,
-//            gpsLong: Double,
-//        ): String {
-//            val apiUrl =
-//                "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCrdntPrxmtSttnList?serviceKey=${apiKey}&pageNo=1&numOfRows=30&_type=json&gpsLati=${gpsLati}&gpsLong=${gpsLong}"
-//            val client = HttpClient.newBuilder().build();
-//            val request = HttpRequest.newBuilder()
-//                .uri(URI.create(apiUrl))
-//                .GET()
-//                .build();
-//
-//            val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-//            return response.body()
-//        }
-//    }
+    class AddressService(
+        private val apiKey: String,
+    ) {
+        fun searchNearStationByGps(
+            gpsLati: Double,
+            gpsLong: Double,
+        ): String {
+            val apiUrl =
+                "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCrdntPrxmtSttnList?serviceKey=${apiKey}&pageNo=1&numOfRows=30&_type=json&gpsLati=${gpsLati}&gpsLong=${gpsLong}"
+            val client = HttpClient.newBuilder().build()
+            val request = HttpRequest.newBuilder()
+                .uri(URI.create(apiUrl))
+                .GET()
+                .build()
+
+            val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+            return response.body()
+        }
+    }
 
     class TerrorlessCrawlingService {
         fun tryCrawling(): TerrorlessData {
-            val client = HttpClient.newBuilder().build();
+            val client = HttpClient.newBuilder().build()
             val request = HttpRequest.newBuilder()
                 .uri(URI.create("https://api.terrorless.01ab.net/trpc/threat.list"))
-                .build();
+                .build()
 
             val response = client.send(request, HttpResponse.BodyHandlers.ofString())
             val print = response.body()
